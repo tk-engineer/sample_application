29c8dde633479a5726c5ae08cfec11e9c78aaefa
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-07-20 07:18:27 +0000

    finish update function

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # get 'lists/show'
   get 'list/:id' => 'lists#show' , as: 'list'
-  get 'lists/edit'
+  # get 'lists/edit'
+  get 'list/:id/edit' => 'lists#edit', as: 'edit_list'
   # get 'homes/top'
+  
+  patch 'list/:id' => 'lists#update', as: 'update_list'
   get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
