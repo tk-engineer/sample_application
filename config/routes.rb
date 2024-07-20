c4e0a2c9069616d74fe61c5901b11864cf4ca9b1
tk226 <69638379+tk-engineer@users.noreply.github.com>
2024-07-20 07:50:29 +0000

    finish delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
+  # get 'lists/new'
+  # post 'lists' => 'lists#create'
   # get 'lists/index'
-  get 'lists' => 'lists#index'
+  # get 'lists' => 'lists#index'
   # get 'lists/show'
-  get 'list/:id' => 'lists#show' , as: 'list'
+  # get 'list/:id' => 'lists#show' , as: 'list'
   # get 'lists/edit'
-  get 'list/:id/edit' => 'lists#edit', as: 'edit_list'
+  # get 'list/:id/edit' => 'lists#edit', as: 'edit_list'
   # get 'homes/top'
   
-  patch 'list/:id' => 'lists#update', as: 'update_list'
+  # patch 'list/:id' => 'lists#update', as: 'update_list'
+  
+  # delete 'list/:id' => 'lists#destroy', as: 'destroy_list'
+  
+  resources :lists
+  
   get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
